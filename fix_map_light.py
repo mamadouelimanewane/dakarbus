a1a86c1e250f7249bcf009b1da8d23e58a9aed12
Mamadou Elimane Wane <mamadouelimane.dia.com>
2026-06-06 13:17:45 +0100

    fix: force light map tiles

---
+++ fix_map_light.py
@@ -0,0 +1,17 @@
+﻿with open('src/components/MapView.tsx', 'r', encoding='utf-8') as f:
+    content = f.read()
+
+# Remplacer TOUTES les URLs de tuiles sombres par des tuiles claires
+content = content.replace(
+    'https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png',
+    'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+)
+
+# S'assurer que darkMode est desactive
+content = content.replace("darkMode: 'dark'", '')
+content = content.replace('darkMode: true', '')
+content = content.replace('darkMode: false', '')
+
+with open('src/components/MapView.tsx', 'w', encoding='utf-8') as f:
+    f.write(content)
+print('MapView.tsx corrige - tuiles claires forcees')
--- src/components/MapView.tsx
+++ src/components/MapView.tsx
@@ -382,7 +382,7 @@ export default function MapView() {
       <MapContainer center={[14.7167, -17.4677]} zoom={12}
         style={{ width: '100%', height: '100%' }} zoomControl={true}>
         <TileLayer
-          url="https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
           attribution=""
         />
         <MapController />
